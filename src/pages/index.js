cba74a6c835a2870be9f437f5359b894f7cf24ef
gwBanzi <61007378+gwBanzi@users.noreply.github.com>
2020-11-19 05:10:08 +0000

    Adjust the image on the home page to be in the center of the page

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,58 +5,10 @@ import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import useBaseUrl from "@docusaurus/useBaseUrl";
 import styles from "./styles.module.css";
+import {Container, Row} from 'react-bootstrap'
 
-const features = [
-  // {
-  //   title: <>Introduction</>,
-  //   imageUrl: "img/product-illustration-01.png",
-  //   imageLinkTo: "docs/product-descriptions/product-overview",
-  //   description: (
-  //     <>
-  //       An introduction to the Glasswall documentation site.
-  //     </>
-  //   ),
-  // },
-  /* {
-    title: <>Glasswall Core</>,
-    imageUrl: "img/documentation-illustration-02.png",
-    imageLinkTo: "docs/product-descriptions/product-overview",
-    description: (
-      <>
-        Documents pertaining to Rebuild, Editor and other core applications.
-      </>
-    ),
-  }, */
-  {
-    title: <>Glasswall Documentation</>,
-    imageUrl: "img/use-cases-illustration-03.png",
-    imageLinkTo: "docs/use-cases/overview",
-    description: (
-      <>
-        A resource for Glasswall documentation.
-      </>
-    ),
-  },
-];
 
-function Feature({ imageUrl, title, description, imageLinkTo }) {
-  const imgUrl = useBaseUrl(imageUrl);
-  return (
-    <div className={classnames("col col--4", styles.feature)}>
-      {imgUrl && (
-        <div className="text--center">
-          <Link to={imageLinkTo}>
-            <img className={styles.featureImage} src={imgUrl} alt={title} />
-          </Link>
-        </div>
-      )}
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </div>
-  );
-}
-
-function Home() {
+function HomePage() {
   const context = useDocusaurusContext();
   const { siteConfig = {} } = context;
   return (
@@ -70,34 +22,23 @@ function Home() {
         <div className="container">
           <h1 className="hero__title">{siteConfig.title}</h1>
           <p className="hero__subtitle">{siteConfig.tagline}</p>
-          {/* <div className={styles.buttons}>
-            <Link
-              className={classnames(
-                "button button--outline button--secondary button--lg",
-                styles.getStarted
-              )}
-              to={useBaseUrl("docs/products/cloud-sdk/rebuild/rebuild-quickstart")}
-            >
-              Get Started
-            </Link>
-          </div> */}
         </div>
       </header>
       <main>
-        {features && features.length && (
-          <section className={styles.features}>
-            <div className="container">
-              <div className="row">
-                {features.map((props, idx) => (
-                  <Feature key={idx} {...props} />
-                ))}
+        <Row>
+          <Container>
+            <div className={classnames('row', styles.responsiveCentered)}>
+              <div className="col col--6 col--offset-3">
+                <img className="homeImage" src="/static/img/use-cases-illustration-03.png"/>
+                <h3 className="">Glasswall Documentation</h3>
+                <p>A resource for Glasswall documentation</p>
               </div>
-            </div>
-          </section>
-        )}
+            </div> 
+          </Container>
+        </Row>
       </main>
     </Layout>
   );
 }
 
-export default Home;
+export default HomePage;
